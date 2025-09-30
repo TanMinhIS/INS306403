45525acf76b9d99f74264e4963c9518c835ca302
Pham Tan Minh <22070977@vnu.edu.vn>
2025-09-30 16:43:25 +0700

    Create Func.php

---
+++ Func.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo strrev(".delrow olleH");
+    
+    $sum = sum(4, 5);
+    echo($sum);
+    function sum($a, $b) {
+        return $a + $b;
+    }
+    ?>
+</body>
+</html>
